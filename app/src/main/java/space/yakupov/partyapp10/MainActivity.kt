ebed23de1ac10d47e4c130c8ab98100852432879
MIshkaV <mavorozhtsov_1@edu.hse.ru>
2021-04-22 11:31:27 +0300

    try to fix fragments

--- app/src/main/java/space/yakupov/partyapp10/MainActivity.kt
+++ app/src/main/java/space/yakupov/partyapp10/MainActivity.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
-    }
-    fun openMap(view: View) {
-        val mapIntent = Intent(this, MapActivity::class.java)
-        startActivity(mapIntent)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener() {
+            val mapIntent = Intent(this, MapActivity::class.java)
+            startActivity(mapIntent)
+        }
     }
 }
