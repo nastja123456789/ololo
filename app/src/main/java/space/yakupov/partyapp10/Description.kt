f2b8d549191fd15bd4049bfc503ca9434cdded28
moskou.nastja.2001@mail.ru <moskou.nastja.2001@mail.ru>
2021-04-22 13:44:51 +0300

    o

--- app/src/main/java/space/yakupov/partyapp10/Description.kt
+++ app/src/main/java/space/yakupov/partyapp10/Description.kt
@@ -12,13 +12,13 @@ class Description : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_description)
-        movie_item_tv_name.text = intent.getStringExtra("title")
-        movie_item_tv_description.text = intent.getStringExtra("context")
-        movie_item_poster.setImageResource(intent.getIntExtra("image", R.drawable.foto1))
+//        movie_item_tv_name.text = intent.getStringExtra("title")
+//        movie_item_tv_description.text = intent.getStringExtra("context")
+//        movie_item_poster.setImageResource(intent.getIntExtra("image", R.drawable.foto1))
 
     }
-
-    override fun onCreateView(): View? {
-        return
-    }
+//
+//    override fun onCreateView(): View? {
+//        return
+//    }
 }
